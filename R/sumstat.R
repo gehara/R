0d4a9d4ecb3c111a756383420040095e1d55a518
gehara <marcelo.gehara@gmail.com>
2017-03-29 16:49:51 -0400

    1

--- R/sumstat.R
+++ R/sumstat.R
@@ -19,9 +19,9 @@ for (j in 1:length(ms.output)){
   pi<-nuc.div(x)*ss/gene.prior[j,5]
   H<-H.div(x)
   TD<-tajima.test(x)$D
-  R2<-R2.test(x,B=0,plot = F,quiet = T)
+  #R2<-R2.test(x,B=0,plot = F,quiet = T)
   spec<-site.spectrum(x)[1:3]
-  SS<-c(pi,ss,H[2],TD,R2,spec)
+  SS<-c(pi,ss,H[2],TD,spec)
   sum.stat<-rbind(sum.stat,SS)
 }
 return(sum.stat)
